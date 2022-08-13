a40faad12d0a5cb5d1bd1a589910d3149f680719
Bruno Grbavac <bruno.grbavac@king-ict.hr>
2022-08-13 13:05:12 +0200

    Added some basic tests.

--- colors-app/src/App.test.tsx
+++ colors-app/src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('Does the logo get rendered?', () => {
+  render(<App/>);
+  expect(screen.getByRole('img', { name: /logo/i })).toBeInTheDocument();
+});
+
+test('Does the text input get rendered?', () => {
+  render(<App/>);
+  expect(screen.getByRole('textbox')).toBeInTheDocument();
+});
+
+test('Does the get color button get rendered?', () => {
+  render(<App/>);
+  expect(screen.getByRole('button', {name: /get a random color!/i})).toBeInTheDocument();
+});
+
+test('Does the random color get rendered?', () => {
+  render(<App/>);
+  userEvent.clear(screen.getByRole('textbox'));
+  userEvent.type(screen.getByRole('textbox'),"color");
+  expect(screen.getByRole('heading', {  name: /color/})).toBeInTheDocument();
+});
+
+test('Does a new list element appear after 2 clicks?', async() => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  userEvent.click(screen.getByRole('button', {name: /get a random color!/i}));
+  userEvent.click(screen.getByRole('button', {name: /get a random color!/i}));
+
+  await waitFor(() => {expect( screen.getAllByRole('article')).toHaveLength(1);});
 });
--- colors-app/src/App.tsx
+++ colors-app/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import logo from './images/logo.svg';
-import './App.css';
 import ColorButton from './components/ColorButton/ColorButton';
 import TextInput from './components/TextInput/TextInput';
 import ColorsList from './components/ColorsList/ColorsList';
 import { invertColor } from './utils/InvertColor';
+import './App.css';
 
 const App:React.FC = () => {
   const [colors, setColors] = useState<string[]>(()=>[]);
@@ -22,7 +22,9 @@ const App:React.FC = () => {
     <div className="App">
         <img src={logo} alt="Logo" height={150}/>
         <h1 id="app-title">Random Color Generator</h1>
-        <h2 id="color-text" style={{color:`#${colors[0]}`, backgroundColor:invertColor(colors[0])}}>{text}</h2>
+        <h2 id="color-text" style={{color:`#${colors[0]}`, backgroundColor:invertColor(colors[0])}}>
+          {text}
+        </h2>
         <TextInput text={text} changeText={changeText}/>
         <ColorButton addColor={addColor}/>
         <ColorsList colors={colors.slice(1)}/>
